c659fcec47198b66a4c4ac0c0e953052da6219e3
EvgenySinyakin93 <evgeny.siniakin83@gmail.com>
2024-03-21 17:54:04 +0300

    2 задача готова

--- src/__tests__/Character.test.js
+++ src/__tests__/Character.test.js
@@ -50,3 +50,51 @@ test("Checking damage", () => {
 
   expect(hero.health).toBeCloseTo(expectedHealth);
 });
+
+import Bowerman from "../js/Bowerman.js";
+import Character from "../js/Character.js";
+
+test("checking Bowerman", () => {
+  const character = new Character("Hero", "Bowerman");
+  const correctCharacter = {
+    name: "Hero",
+    type: "Bowerman",
+    health: 100,
+    level: 1,
+  };
+  expect(character).toEqual(correctCharacter);
+});
+
+test("Checking name", () => {
+  expect(() => new Character("B", "Bowerman")).toThrow(
+    "Длина имени персонажа не корректна.Выберите имя от 2 до 10 символов"
+  );
+  expect(() => new Character("BowermanVeryLength", "Bowerman")).toThrow(
+    "Длина имени персонажа не корректна.Выберите имя от 2 до 10 символов"
+  );
+});
+
+test("Checking Hero", () => {
+  expect(() => new Character("NaN", "Hero")).toThrow("Персонаж не существует");
+});
+
+test("Checking Level умершего", () => {
+  expect(() => {
+    const bowerman = new Bowerman("Bowerman");
+    bowerman.health = -15;
+    bowerman.levelUp();
+  }).toThrow("Нельзя повысить левел умершего"); 
+});
+
+
+test("Checking damage", () => {
+  const bowerman = new Bowerman("Bowerman");
+  bowerman.damage(10);
+  expect(bowerman.health).toBe(92.5);
+});
+
+test("Checking damage health is 0", () => {
+  const bowerman = new Bowerman("Bowerman");
+  bowerman.damage(140);
+  expect(bowerman.health).toBe(-5);
+});
--- src/js/Character.js
+++ src/js/Character.js
@@ -1,44 +1,44 @@
 export default class Character {
-    constructor(name, type) {
-        if (name.length < 2 || name.length > 10) {
-            throw new Error(
-                "Длина имени персонажа не корректна.Выберите имя от 2 до 10 символов"
-            );
-        }
-
-        const types = [
-            "Bowerman",
-            "Swordsman",
-            "Magician",
-            "Daemon",
-            "Undead",
-            "Zombie",
-        ];
-        if (!types.includes(type)) {
-            throw new Error("Персонаж не существует");
-        }
-
-        this.name = name;
-        this.type = type;
-        this.health = 100;
-        this.level = 1;
+  constructor(name, type) {
+    if (name.length < 2 || name.length > 10) {
+      throw new Error(
+        "Длина имени персонажа не корректна.Выберите имя от 2 до 10 символов"
+      );
     }
 
-    levelUp() {
-        if (this.health >= 0) {
-            this.level += 1;
-            this.attack *= 1.2;
-            this.defence *= 1.2;
-            this.health = 100;
-        } else {
-            throw new Error("Нельзя повысить левел умершего");
-        }
+    const types = [
+      "Bowerman",
+      "Swordsman",
+      "Magician",
+      "Daemon",
+      "Undead",
+      "Zombie",
+    ];
+    if (!types.includes(type)) {
+      throw new Error("Персонаж не существует");
     }
 
-    damage(points) { 
-        if (this.health >= 0) {
-            const damage = points * (1 - this.defence / 100);
-            this.health -= damage;
-        }
+    this.name = name;
+    this.type = type;
+    this.health = 100;
+    this.level = 1;
+  }
+
+  levelUp() {
+    if (this.health >= 0) {
+      this.level += 1;
+      this.attack *= 1.2;
+      this.defence *= 1.2;
+      this.health = 100;
+    } else {
+      throw new Error("Нельзя повысить левел умершего");
     }
+  }
+
+  damage(points) {
+    if (this.health >= 0) {
+      const damage = points * (1 - this.defence / 100);
+      this.health -= damage;
+    }
+  }
 }
